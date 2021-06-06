5df714689321451fc65f32d518a85905bac7b74e
Lucas Xavier <lucasxaviert875@gmail.com>
2021-06-06 17:04:12 -0300

    Inserindo arquivos do professor

--- trie.h
+++ trie.h
@@ -1,6 +1,7 @@
 struct trie{
     char letra;
     int termino;
+    
     struct trie *filhos[26];
     int ocupacao;
 };
@@ -9,18 +10,13 @@ typedef struct trie Trie;
 
 Trie *criaNo(char v);
 
+Trie *criaTrie();
 void inserePalavra(Trie *t, char *palavra);
-
 int buscarPalavra(Trie *t, char *palavra);
-
 Trie* buscarPrefixo(Trie *t, char *palavra);
-
 void removerPalavra(Trie *t, char *palavra);
 
-Trie *criaTrie();
-
 void alphabetize(Trie * t);
-
-void alphabetize2(Trie * t, char prefixo[])
+void alphabetize2(Trie * t, char str[]);
 
 void liberar(Trie *t);
